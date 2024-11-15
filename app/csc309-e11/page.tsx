a42a8c6d910e464a72d49340eba3218ebcff7a60
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-11-14 23:28:20 -0500

    test

---
+++ app/csc309-e11/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const Csc309E11 = () => {
+    return (
+        <div id="auto-tester">
+            Hi. I am lekevi15, and I have learned how to develop a full-stack web app
+            and deploy it!
+        </div>
+    )
+}
+
+export default Csc309E11
